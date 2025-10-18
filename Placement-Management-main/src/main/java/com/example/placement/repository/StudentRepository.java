49170347f2e24b160b7ec6b6217ecb0d732695d7
S. Abinaya <abiss9705@gmail.com>
2025-10-18 17:15:05 +0530

    Delete Placement-Management-main directory

--- Placement-Management-main/src/main/java/com/example/placement/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.placement.repository;
-
-import com.example.placement.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByBatchId(Long batchId);
-}
